fd8bb3dfbc9226c596f3b633ba63cf941a1a1abb
ashfiaKhatun <ashfia1204@gmail.com>
2024-06-10 22:28:25 +0600

    connected to localhost server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+        ]
+    })
+);
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send("Tourist Guide is running");
+})
+app.listen(port, () => {
+    console.log(`Server running in port: ${port}`);
+})

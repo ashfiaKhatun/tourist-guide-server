873ee8e26f3cda24b0d99adcf85f8b804b1b47ff
ashfiaKhatun <ashfia1204@gmail.com>
2024-06-19 00:17:32 +0600

    added middleware

--- index.js
+++ index.js
@@ -41,6 +41,22 @@ async function run() {
             res.send({ token });
         })
 
+        // middlewares
+        const verifyToken = (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+                if (error) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
         
 
 

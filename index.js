42e584d17c6bc1dee45941b1dd3b42a3f87a7a60
ashfiaKhatun <ashfia1204@gmail.com>
2024-06-19 00:18:15 +0600

    added another middleware

--- index.js
+++ index.js
@@ -57,6 +57,18 @@ async function run() {
             })
         }
 
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+
+            const isAdmin = user?.role === 'Admin'
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next();
+        }
+
         
 
 

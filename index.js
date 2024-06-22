0ee252216889181bcbab3594f4b427bf98353da6
ashfiaKhatun <ashfia1204@gmail.com>
2024-06-22 22:17:22 +0600

    hosting complete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
@@ -10,6 +10,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
+            "https://tourist-guide-d1f39.web.app"
         ]
     })
 );
@@ -31,6 +32,9 @@ async function run() {
     try {
 
         const usersCollection = client.db("tourGuideDB").collection("users");
+        const packageCollection = client.db("tourGuideDB").collection("tourPackages");
+        const bookingsCollection = client.db("tourGuideDB").collection("bookedPackage");
+        const storyCollection = client.db("tourGuideDB").collection("stories");
 
 
         // jwt related api
@@ -92,6 +96,37 @@ async function run() {
         })
 
 
+        // get Tourist Guide
+        app.get('/users/guide/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let guide = false;
+            if (user) {
+                guide = user?.role === 'Tourist Guide'
+            }
+            res.send({ guide })
+        })
+
+        // get Tourist
+        app.get('/users/tourist/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let tourist = false;
+            if (user) {
+                tourist = user?.role === 'Tourist'
+            }
+            res.send({ tourist })
+        })
+
+
 
         app.post("/users", async (req, res) => {
             const user = req.body;
@@ -103,6 +138,90 @@ async function run() {
             }
 
             const result = await usersCollection.insertOne(user);
+            res.send(result)
+        })
+
+        // make Tourist Guide
+        app.patch('/users/guide/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+
+            const updatedDoc = {
+                $set: {
+                    role: 'Tourist Guide'
+                }
+            }
+
+            const result = await usersCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+        // get guides 
+
+        app.get('/users/guides', async (req, res) => {
+            const result = await usersCollection.find({ role: "Tourist Guide" }).toArray();
+            res.send(result)
+        })
+
+
+
+        // make admin
+        app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+
+            const updatedDoc = {
+                $set: {
+                    role: 'Admin'
+                }
+            }
+
+            const result = await usersCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+
+
+        // Tour packages
+
+        app.get('/tour-package', async (req, res) => {
+            const result = await packageCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.get('/tour-package/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await packageCollection.findOne(query);
+            res.send(result)
+        })
+
+        app.post("/tour-package", verifyToken, verifyAdmin, async (req, res) => {
+            const packages = req.body;
+            const result = await packageCollection.insertOne(packages);
+            res.send(result)
+        })
+
+
+        // Bookings
+
+        app.post("/bookings", verifyToken, async (req, res) => {
+            const packages = req.body;
+            const result = await bookingsCollection.insertOne(packages);
+            res.send(result)
+        })
+
+
+        //get stories
+        app.get('/story', async (req, res) => {
+            const result = await storyCollection.find().toArray();
+            res.send(result)
+        })
+
+        // Upload Story
+        app.post("/story", verifyToken, async (req, res) => {
+            const story = req.body;
+            const result = await storyCollection.insertOne(story);
             res.send(result)
         })
 
